fc90a4cf96da1a4c41f49a51ee600de11ce29905
Jared Hatfield <jjhatf02@gmail.com>
2024-04-03 19:03:27 -0400

    DocumentResourceNameUtil to help with extracting relevant attributes

---
+++ src/main/java/com/unitvectory/crossfiresync/DocumentResourceNameUtil.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.unitvectory.crossfiresync;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * The DocumentResourceNameUtil utility class.
+ * 
+ * @author Jared Hatfield (UnitVectorY Labs)
+ */
+public class DocumentResourceNameUtil {
+
+    // Compile the Pattern once; Pattern is thread-safe
+    private static final Pattern DOCUMENT_PATH_PATTERN = Pattern.compile("projects/.*/databases/.*/documents/(.+)");
+
+    /**
+     * Extracts the document_path from the resource name using a Matcher.
+     * 
+     * The resource name in the format of
+     * `projects/{project_id}/databases/{database_id}/documents/{document_path}`
+     * will have just the `{document_path}` returned.
+     * 
+     * @param resourceName the resource name
+     * @return the document path
+     */
+    public static String getDocumentPath(String resourceName) {
+        // Create a new Matcher from the static Pattern for each call; Matcher is not
+        // thread-safe
+        Matcher matcher = DOCUMENT_PATH_PATTERN.matcher(resourceName);
+
+        if (matcher.find()) {
+            // Return the captured group which is the document path
+            return matcher.group(1);
+        }
+
+        // No match found, return null or throw an exception
+        return null;
+    }
+}
---
+++ src/test/java/com/unitvectory/crossfiresync/DocumentResourceNameUtilDocumentPathTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.unitvectory.crossfiresync;
+
+import org.junit.jupiter.params.ParameterizedTest;
+
+import com.unitvectory.fileparamunit.ListFileSource;
+import com.unitvectory.jsonparamunit.JsonClassParamUnit;
+
+/**
+ * The DocumentResourceNameDocumentPathUtil utility class test.
+ * 
+ * @author Jared Hatfield (UnitVectorY Labs)
+ */
+public class DocumentResourceNameUtilDocumentPathTest extends JsonClassParamUnit<TestRecord, TestRecord> {
+
+    protected DocumentResourceNameUtilDocumentPathTest() {
+        super(TestRecord.class);
+    }
+
+    @ParameterizedTest
+    @ListFileSource(resources = "/documentPath/", fileExtension = ".json", recurse = false)
+    public void testIt(String file) {
+        run(file);
+    }
+
+    @Override
+    protected TestRecord process(TestRecord input, String context) {
+        return TestRecord.builder().value(DocumentResourceNameUtil.getDocumentPath(input.getValue())).build();
+    }
+}
